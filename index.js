e1b07d7c21107dee8830302755ae9a6a7c87a2c9
beeboo197 <160324997+beeboo197@users.noreply.github.com>
2024-02-18 19:58:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import { Bot,webhookCallback } from "grammy";
-import fetch from "node-fetch";
-import {} from 'dotenv/config'
+const { Bot, webhookCallback } = require("grammy");
+const fetch = require("node-fetch");
+const express = require("express");
+require("dotenv").config();
 const bot = new Bot(process.env.BOT_TOKEN);
 
 
